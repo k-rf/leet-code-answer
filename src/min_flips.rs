29cee79884047313c7e924b5a1c1caaecbbf1b08
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-06-07 12:05:01 +0000

    🚀 Add another answer

--- src/min_flips.rs
+++ src/min_flips.rs
@@ -4,29 +4,13 @@ impl Solution {
     pub fn min_flips(mut a: i32, mut b: i32, mut c: i32) -> i32 {
         let mut ans = 0;
 
-        let mut max = a.max(b.max(c));
-        let mut len = 0;
-
-        while 0 < max {
-            max >>= 1;
-            len += 1;
-        }
-
-        for _ in 0..len {
-            let (x, y, z) = (a & 1, b & 1, c & 1);
-
-            match (x, y, z) {
-                (1, 1, 1) => (),
-                (1, 0, 1) => (),
-                (0, 1, 1) => (),
-                (0, 0, 1) => ans += 1,
-
+        for _ in 0..(a.max(b.max(c)) as f32).log2() as i32 {
+            match (a & 1, b & 1, c & 1) {
+                (0, 0, 0) | (1, _, 1) | (_, 1, 1) => (),
                 (1, 1, 0) => ans += 2,
-                (1, 0, 0) => ans += 1,
-                (0, 1, 0) => ans += 1,
-                (0, 0, 0) => (),
-                _ => (),
+                _ => ans += 1,
             }
+
             a >>= 1;
             b >>= 1;
             c >>= 1;
