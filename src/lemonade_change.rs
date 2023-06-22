ed3ca3cabb21ae7833a5204b723e1b424e1b0996
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-06-22 15:04:24 +0000

    🚀 Update lemonade_change.rs
    この規模なら `HashMap` も要らないかな。

--- src/lemonade_change.rs
+++ src/lemonade_change.rs
@@ -1,33 +1,30 @@
-use std::collections::HashMap;
-
 struct Solution;
 
 impl Solution {
     pub fn lemonade_change(bills: Vec<i32>) -> bool {
-        let mut revenue = HashMap::from([(5, 0), (10, 0)]);
+        let (mut b5, mut b10) = (0, 0);
 
         for bill in bills {
             match bill {
                 5 => {
-                    revenue.entry(5).and_modify(|c| *c += 1);
+                    b5 += 1;
                 }
                 10 => {
-                    revenue.entry(10).and_modify(|c| *c += 1);
-                    revenue.entry(5).and_modify(|c| *c -= 1);
+                    b10 += 1;
+                    b5 -= 1;
                 }
                 20 => {
-                    let b10 = revenue.get_mut(&10).unwrap();
-                    if 0 < *b10 {
-                        *b10 -= 1;
-                        revenue.entry(5).and_modify(|c| *c -= 1);
+                    if 0 < b10 {
+                        b10 -= 1;
+                        b5 -= 1;
                     } else {
-                        revenue.entry(5).and_modify(|c| *c -= 3);
+                        b5 -= 3;
                     }
                 }
                 _ => (),
             }
 
-            if !revenue.values().all(|&x| 0 <= x) {
+            if b5 < 0 || b10 < 0 {
                 return false;
             }
         }
