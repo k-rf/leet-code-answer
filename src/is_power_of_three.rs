0d2d6e0f3caaa277d3610b748ae63d30bea1ddc2
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-06-28 16:30:37 +0000

    🚀 Update is_power_of_three.rs

--- src/is_power_of_three.rs
+++ src/is_power_of_three.rs
@@ -2,14 +2,6 @@ struct Solution {}
 
 impl Solution {
     pub fn is_power_of_three(mut n: i32) -> bool {
-        if n < 1 {
-            return false;
-        }
-
-        while n % 3 == 0 {
-            n /= 3;
-        }
-
-        n == 1
+        1 < n && 3_i32.pow((i32::MAX as f64).log(3.0) as u32) % n == 0
     }
 }
